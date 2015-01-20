b200c888bcf873222dce8e2ac2b6af4805b4e432
lschmischke <kisef@web.de>
2015-01-20 15:19:01 +0100

    jquery for wiki

--- js/wikipedia.js
+++ js/wikipedia.js
@@ -1,30 +1,38 @@
 $(document).ready(function() {
+	var max_zeichen=30;
 	$(".button").click(function() {
 		var item =$('#wiki').val();
 		if(item==="") {
 			$('#wiki').addClass('highlighted');
-			alert("no empty search please");
+			$('.checkInput').replaceWith("<p class=\"checkInput wrong\">Keine leere Suche erlaubt!");
+		}
+		else if(item.length>max_zeichen) {
+			$('#wiki').addClass('highlighted');
 		}
 		else{
-			$('.liste').append('<li class="item">'+item+'</li>');
 			function OpenInNewTab(url) {
 				  var win = window.open(url, '_blank');
 				  win.focus();
 				};
 			OpenInNewTab("http://de.wikipedia.org/wiki/Spezial:Suche?search="+item+"&go=Artikel")
-			
+		}
+	});
+	$('#wiki').on('input',function() {
+		var val = max_zeichen-$('#wiki').val().length;
+		if(val<0) {
+			$('#wiki').addClass('highlighted');
+			$('.checkInput').replaceWith("<p class=\"checkInput wrong\">Ihre Sucheingabe überschreitet die erlaubte Länge um: "+(val*-1)+ " Zeichen");
+		}
+		else if (val === max_zeichen) {
+			$('#wiki').addClass('highlighted');
+			$('.checkInput').replaceWith("<p class=\"checkInput wrong\">Keine leere Suche erlaubt!");		
+		}
+		else {
+			$('#wiki').removeClass('highlighted');
+			$('.checkInput').replaceWith("<p class=\"checkInput\">Noch erlaubte Zeichen: "+val);
 		}
 	});
 	$(document).on('click','.item',function() {
 		$(this).remove();
 	})
-	$('#wiki').on('input',function() {
-		var input = $('#wiki').val();
-		if(! /^[a-zA-Z0-9 ]+$/.test(input)) {
-			$('wiki').addClass('highlighted');
-		}
-		else {
-			$('wiki').removeClass('highlighted');
-		}
-	});
 })
